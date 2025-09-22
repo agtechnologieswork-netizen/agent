1217e58324d3d9fe86095cfb3365126dcd5537bb
Igor Rekun <igrekun@gmail.com>
2025-09-22 15:15:32 +0100

    Unified processor trait. (#441)

--- dabgent/dabgent_agent/examples/basic.rs
+++ dabgent/dabgent_agent/examples/basic.rs
@@ -1,76 +1,57 @@
-use dabgent_agent::agent::{self};
-use dabgent_agent::handler::Handler;
-use dabgent_agent::thread::{self};
+use dabgent_agent::processor::{Pipeline, Processor, ThreadProcessor, ToolProcessor};
 use dabgent_agent::toolbox::{self, basic::toolset};
-use dabgent_mq::EventStore;
-use dabgent_mq::db::{Query, sqlite::SqliteStore};
+use dabgent_mq::{EventStore, db::sqlite::SqliteStore};
 use dabgent_sandbox::dagger::{ConnectOpts, Sandbox as DaggerSandbox};
 use dabgent_sandbox::{Sandbox, SandboxDyn};
 use eyre::Result;
 use rig::client::ProviderClient;
 
+const MODEL: &str = "claude-sonnet-4-20250514";
+
+const SYSTEM_PROMPT: &str = "
+You are a python software engineer.
+Workspace is already set up using uv init.
+Use uv package manager if you need to add extra libraries.
+Program will be run using uv run main.py command.
+";
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    run().await;
+
+    const STREAM_ID: &str = "pipeline";
+    let prompt = "minimal script that fetches my ip using some api like ipify.org";
+
+    let store = store().await;
+    push_prompt(&store, STREAM_ID, "", prompt).await.unwrap();
+    pipeline_fn(STREAM_ID, store).await.unwrap();
 }
 
-async fn run() {
+pub async fn pipeline_fn(stream_id: &str, store: impl EventStore) -> Result<()> {
+    let stream_id = stream_id.to_owned();
     let opts = ConnectOpts::default();
     opts.connect(|client| async move {
         let llm = rig::providers::anthropic::Client::from_env();
         let sandbox = sandbox(&client).await?;
-        let store = store().await;
-
         let tools = toolset(Validator);
-        let llm_worker = agent::Worker::new(
-            llm,
+
+        let thread_processor = ThreadProcessor::new(
+            llm.clone(),
             store.clone(),
-            "claude-sonnet-4-20250514".to_owned(),
+            MODEL.to_owned(),
             SYSTEM_PROMPT.to_owned(),
             tools.iter().map(|tool| tool.definition()).collect(),
         );
-
-        let tools = toolset(Validator);
-        let mut sandbox_worker = agent::ToolWorker::new(sandbox.boxed(), store.clone(), tools);
-
-        tokio::spawn(async move {
-            let _ = llm_worker.run("basic", "thread").await;
-        });
-        tokio::spawn(async move {
-            let _ = sandbox_worker.run("basic", "thread").await;
-        });
-
-        let event = thread::Event::Prompted(
-            "minimal script that fetches my ip using some api like ipify.org".to_owned(),
+        let tool_processor = ToolProcessor::new(sandbox.boxed(), store.clone(), tools);
+        let pipeline = Pipeline::new(
+            store.clone(),
+            vec![thread_processor.boxed(), tool_processor.boxed()],
         );
-        store
-            .push_event("basic", "thread", &event, &Default::default())
-            .await?;
-
-        let query = Query {
-            stream_id: "basic".to_owned(),
-            event_type: None,
-            aggregate_id: Some("thread".to_owned()),
-        };
-
-        let mut receiver = store.subscribe::<thread::Event>(&query)?;
-        let mut events = store.load_events(&query, None).await?;
-        while let Some(event) = receiver.next().await {
-            let event = event?;
-            events.push(event.clone());
-            let thread = thread::Thread::fold(&events);
-            tracing::info!(?thread.state, ?event, "event");
-            match thread.state {
-                thread::State::Done => break,
-                _ => continue,
-            }
-        }
-
+        pipeline.run(stream_id.clone()).await?;
         Ok(())
     })
     .await
-    .unwrap();
+    .map_err(Into::into)
 }
 
 async fn sandbox(client: &dagger_sdk::DaggerConn) -> Result<DaggerSandbox> {
@@ -94,12 +75,18 @@ async fn store() -> SqliteStore {
     store
 }
 
-const SYSTEM_PROMPT: &str = "
-You are a python software engineer.
-Workspace is already set up using uv init.
-Use uv package manager if you need to add extra libraries.
-Program will be run using uv run main.py command.
-";
+async fn push_prompt<S: EventStore>(
+    store: &S,
+    stream_id: &str,
+    aggregate_id: &str,
+    prompt: &str,
+) -> Result<()> {
+    let event = dabgent_agent::event::Event::Prompted(prompt.to_owned());
+    store
+        .push_event(stream_id, aggregate_id, &event, &Default::default())
+        .await
+        .map_err(Into::into)
+}
 
 pub struct Validator;
 
---
+++ dabgent/dabgent_agent/src/event.rs
@@ -0,0 +1,34 @@
+use crate::llm::CompletionResponse;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Event {
+    LLMConfig {
+        model: String,
+        temperature: f64,
+        max_tokens: u64,
+        preamble: Option<String>,
+        tools: Option<Vec<rig::completion::ToolDefinition>>,
+        recipient: Option<String>,
+    },
+    AgentMessage {
+        response: CompletionResponse,
+        recipient: Option<String>,
+    },
+    UserMessage(rig::OneOrMany<rig::message::UserContent>),
+    ArtifactsCollected(HashMap<String, String>),
+}
+
+impl dabgent_mq::Event for Event {
+    const EVENT_VERSION: &'static str = "1.0";
+
+    fn event_type(&self) -> &'static str {
+        match self {
+            Event::LLMConfig { .. } => "llm_config",
+            Event::AgentMessage { .. } => "agent_message",
+            Event::UserMessage(..) => "user_message",
+            Event::ArtifactsCollected(..) => "artifacts_collected",
+        }
+    }
+}
